e2ef90562244969296f3463402d658f280843f2c
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2022-12-17 11:59:30 +0000

    ヘッダーとフッター作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< Updated upstream
-=======
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -16,5 +15,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :tel_number])
   end
->>>>>>> Stashed changes
+
 end
