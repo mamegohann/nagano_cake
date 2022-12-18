a8f52b7b27aea6cb8b39e5d6a9fd4b2613dc1a1d
rts020727 <rts020727@gmail.com>
2022-12-18 16:05:57 +0000

    add destinations view

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -2,18 +2,30 @@ class Public::DestinationsController < ApplicationController
   
   def index
     @destination = Destination.new
+    @destinations = Destination.all
   end
 
   def edit
+    @destination = Destination.find(params[:id])
   end
   
   def create
+    destination = Destination.new
+    destination.customer_id = current_customer.id
+    destination.save
+    redirect_to destinations_path
   end
   
   def update
+    destination = Destination.find(params[:id])
+    destination.update
+    redirect_to destinations_path
   end
   
   def destroy
+    destination = Destination.find(params[:id])
+    destination.destroy
+    redirect_to destinations_path
   end
   
 end
