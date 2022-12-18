8793ade01f8e6c0a82799c1503b41bdfbcef3f78
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-18 05:08:24 +0000

    ジャンルモデル・コントローラー・view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,28 +1,40 @@
-class Public::OrdersController < ApplicationController
+lass Public::OrdersController < ApplicationController
   
   def new
     @order = Order.new
-  end
-  
-  def check
-  end
-  
-  def over
-  end
-  
-  def create
-  end
-  
-  def index
-  end
-  
-  def show
-  end
+	end
+
+	def check
+	  @cart_items = current_cart
+		@order = Order.new(order_params)
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+	end
+
+	def over
+	end
+
+	def create
+    @order = current_customer.orders.new(order_params)
+    @order.save
+    @cart_items = current_cart
+    @cart_items.all_destroy# 注文確定後カートを空にする
+	end
+
+	def index
+    @orders = current_customer.orders
+	end
+
+	def show
+	  @order = Order.find(params[:id])
+	end
   
   private
   
   def order_params
-    params.require(:order).permit(:quantity,:item_id)
+    params.require(:order).permit(:quantity, :item_id, :payment_method)
   end
   
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_074322) do
+ActiveRecord::Schema.define(version: 2022_12_18_001916) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,6 +85,12 @@ ActiveRecord::Schema.define(version: 2022_12_16_074322) do
     t.string "destination_name", null: false
     t.string "destination_postal_code", null: false
     t.string "destination_address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
