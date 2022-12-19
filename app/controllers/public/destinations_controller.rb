12d536edd36e2035ab91031b7f279462b19ee414
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-19 05:34:29 +0000

    コンフリクト解消

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,5 +1,5 @@
 class Public::DestinationsController < ApplicationController
-  
+
   def index
     @destination = Destination.new
     @destinations = Destination.all
@@ -8,24 +8,24 @@ class Public::DestinationsController < ApplicationController
   def edit
     @destination = Destination.find(params[:id])
   end
-  
+
   def create
     destination = Destination.new
     destination.customer_id = current_customer.id
     destination.save
     redirect_to destinations_path
   end
-  
+
   def update
     destination = Destination.find(params[:id])
     destination.update
     redirect_to destinations_path
   end
-  
+
   def destroy
     destination = Destination.find(params[:id])
     destination.destroy
     redirect_to destinations_path
   end
-  
+
 end
