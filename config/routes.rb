c04e76b5cb9fe21d376c9bb42fd238606fcdbaad
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2022-12-20 10:20:56 +0000

    admin/customersコントローラ、ビュー追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  
+
   namespace :public do
     get 'customers/show'
   end
@@ -16,10 +16,10 @@ Rails.application.routes.draw do
   devise_for :admin,skip: [:registrations, :passwords],controllers: {
     sessions: "admin/sessions"
   }
-  
+
   # 会員側のルーティング設定
   scope module: :public do
-    resources :items, only: [:index, :show] 
+    resources :items, only: [:index, :show]
     resources :cart_items, only: [:index,:create,:update,:destroy] do
       collection do
         delete "all_destroy"   #パスが　all_destroy_cart_items_path, method: :delete　となる
@@ -37,14 +37,14 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
   namespace :admin do
     resources :genres
     resources :items,only: [:index,:new,:create,:show,:edit,:update,]
   end
-  
+
   namespace :admin do
     resources :customers, only: [:index,:show,:edit,:update]
   end
-  
+
 end
