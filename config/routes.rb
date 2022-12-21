bfb4759fa9bb074b95bda69bf7260d46fe8aa801
rts020727 <rts020727@gmail.com>
2022-12-21 00:07:33 +0000

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  
-  namespace :admin do
-    get 'orders/show'
-  end
   namespace :public do
     get 'customers/show'
   end
@@ -46,6 +42,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :customers, only: [:index,:show,:edit,:update]
+    resources :orders, only: [:show]
   end
   
 end
