ad8e52bb8564e24e00ae4b33cd9fd498856f6c2e
rts020727 <rts020727@gmail.com>
2022-12-21 06:33:12 +0000

    プル

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,6 +3,7 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8)
     @genres = Genre.all
+    @genre = Genre.find_by(params[:genre_id])
   end
 
   def show
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ApplicationRecord
   has_many :cart_items
 
   has_many :destinations
-         
+
   def active_for_authentication?
     super && (status == false)
   end
