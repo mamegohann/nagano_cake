bb61addd50d1d6f163b5ed3599ddd0f75b9d1f8c
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-23 05:54:32 +0000

    orderのindexページの修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -59,7 +59,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_customer.orders.all
+    @orders = current_customer.orders.page(params[:page])
   end
 
   def show
