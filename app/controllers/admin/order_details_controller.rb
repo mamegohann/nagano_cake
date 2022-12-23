12bab6dc2b5520c1a9cd45c041c8b0ce47f39b33
rts020727 <rts020727@gmail.com>
2022-12-23 13:48:39 +0000

    制作ステータス編集

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,7 @@ class Admin::OrderDetailsController < ApplicationController
     order_detail.update(order_detail_params)
     if order_detail.status == "production"
       order_detail.order.update(status: "production")
-    elsif order_detail.status == "complete_production"
+    elsif order_detail.order.order_details.all?{|order_detail| order_detail.status == "complete_production"}
       order_detail.order.update(status: "shipping_preparation")
     end
     redirect_to admin_order_path(order_detail.order_id)
