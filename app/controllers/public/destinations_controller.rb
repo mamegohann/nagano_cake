c92cc8bb18c4329cff34a774f5c2f2ae5f805cdd
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-24 15:25:22 +0000

    フラッシュメッセージとレイアウト修正

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -3,7 +3,7 @@ class Public::DestinationsController < ApplicationController
   
   def index
     @destination = Destination.new
-    @destinations = Destination.all
+    @destinations = Destination.where(customer:current_customer)
   end
 
   def edit
