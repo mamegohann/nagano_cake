f0ec1bd57651c332a70e818915290c1f60b7fb95
rts020727 <rts020727@gmail.com>
2022-12-24 04:09:01 +0000

    リンク修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,21 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-
     if customer_signed_in?
-      customers_path
+      root_path
     elsif admin_signed_in?
       admin_orders_path
-    else
     end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    elsif resource_or_scope == :customer
+      root_path
+    else
+      root_path
+    end
   end
 
   def configure_permitted_parameters
